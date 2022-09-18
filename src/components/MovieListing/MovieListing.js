27316789bbef77235a6441d8e7f53b64f3eb5cc7
Rahul Sharma <rahulsharmabhumca16@gmail.com>
2022-09-18 12:14:42 +0000

    added slick

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -3,8 +3,16 @@ import { useSelector } from "react-redux";
 import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
+import Slider from "react-slick";
 
 const MovieListing = () => {
+  const settings = {
+    dots : false,
+    infinite : true,
+    speed : 500,
+    slidesToShow : 6,
+    slidesToScroll: 3,
+  }
   const movies = useSelector(getAllMovies);
   const shows = useSelector(getAllShows);
   console.log(shows);
@@ -35,7 +43,11 @@ const MovieListing = () => {
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">{renderMovies}</div>
+        <div className="movie-container">
+          <Slider {...settings }>
+          {renderMovies}
+          </Slider>
+        </div>
       </div>
       <div className="movie-list">
         <h2>Shows</h2>
