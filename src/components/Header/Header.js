c9318a444d9622e0c46f3d448e03ce8e20338561
Rahul Sharma <rahulsharmabhumca16@gmail.com>
2022-09-18 19:04:30 +0530

    added search functionality...

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { fetchAsyncMovies, fetchAsyncShows } from "../../features/movies/movieSlice";
 import user from "../../images/user.png";
-import "./Header.scss"
+import "./Header.scss";
 
 const Header = () => {
+  const [term,setTerm] = useState("");
+  const dispatch = useDispatch()
+  const submithandler = (e) => {
+    e.preventDefault();
+    if(term === ""){
+      return alert("Please enter search term !")
+    }
+    dispatch(fetchAsyncMovies(term));
+    dispatch(fetchAsyncShows(term));
+    setTerm("");
+  }
   return (
-    <div className='header'>
-     <Link to="/">
-        <div className='logo'>Movie App</div>
-     </Link>
-     <div className='user-image'>
-      <img src={user} alt="user" />
-     </div>
+    <div className="header">
+      <div className="logo">
+        <Link to="/">Movie App </Link>
+      </div>
+      <div>
+        <form onSubmit={submithandler}>
+          <input type="text" value={term} placeholder="Search Movies or Shows" onChange={(e) => setTerm(e.target.value)} />
+          <button type="submit"><i className="fa fa-search"></i></button>
+        </form>
+      </div>
+      <div className="user-image">
+        <img src={user} alt="user" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -3,16 +3,8 @@ import { useSelector } from "react-redux";
 import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
-import Slider from "react-slick";
 
 const MovieListing = () => {
-  const settings = {
-    dots : false,
-    infinite : true,
-    speed : 500,
-    slidesToShow : 6,
-    slidesToScroll: 3,
-  }
   const movies = useSelector(getAllMovies);
   const shows = useSelector(getAllShows);
   console.log(shows);
@@ -44,14 +36,14 @@ const MovieListing = () => {
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">
-          <Slider {...settings }>
           {renderMovies}
-          </Slider>
         </div>
       </div>
       <div className="movie-list">
         <h2>Shows</h2>
-        <div className="movie-container">{renderShows}</div>
+        <div className="movie-container">
+          {renderShows}
+        </div>
       </div>
     </div>
   );
